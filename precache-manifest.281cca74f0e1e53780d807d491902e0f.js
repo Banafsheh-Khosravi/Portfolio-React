faccaad58e5a2763a5dc1e8a5565e269b0c5de58
Bani <bani@Banis-MacBook-Pro.local>
2019-09-25 17:28:42 -0400

    Updates

---
+++ precache-manifest.281cca74f0e1e53780d807d491902e0f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b01f6180b222049735f8e9db87a058e",
+    "url": "/Portfolio-React/index.html"
+  },
+  {
+    "revision": "5b6347b06bad05460473",
+    "url": "/Portfolio-React/static/css/main.7c1b9e87.chunk.css"
+  },
+  {
+    "revision": "b20b707fe6d533560229",
+    "url": "/Portfolio-React/static/js/2.f61f0b6e.chunk.js"
+  },
+  {
+    "revision": "5b6347b06bad05460473",
+    "url": "/Portfolio-React/static/js/main.6f29a748.chunk.js"
+  },
+  {
+    "revision": "f2cdbc1d4fc5e99e2d49",
+    "url": "/Portfolio-React/static/js/runtime-main.0b61749b.js"
+  },
+  {
+    "revision": "aac6b196921d1707c1d583d9ac0323bb",
+    "url": "/Portfolio-React/static/media/image1.aac6b196.png"
+  }
+]);
